d0d31b244c9401f3e17d5a249ab87fb3b36dba57
Sunil-731820 <sunilkumargupta1170@gmail.com>
2024-09-28 21:51:20 +0530

    projects completed

--- LoginAndSignUpWithForgotPasswordExample/src/main/java/com/java/spr/controller/RegisterLoginController.java
+++ LoginAndSignUpWithForgotPasswordExample/src/main/java/com/java/spr/controller/RegisterLoginController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.java.spr.DTO.UserDTO;
 import com.java.spr.DTO.UserLoginDTO;
