25838888c9fb2f216d527eb85d9481530142eed3
Sunil-731820 <sunilkumargupta1170@gmail.com>
2024-12-04 20:04:47 +0530

    Added The code for the API GateWay method

---
+++ APIGatewayUsingAPIGateWay/APIGatewayUsingAPIGateWay/src/main/java/com/java/spr/ApiGatewayUsingApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.java.spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayUsingApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayUsingApiGateWayApplication.class, args);
+	}
+
+}
---
+++ UserServiceUsingAPIGateWay/src/main/java/com/java/spr/UserServiceUsingApiGateWayApplication.java
@@ -0,0 +1,13 @@
+package com.java.spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceUsingApiGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceUsingApiGateWayApplication.class, args);
+	}
+
+}
