43b25fb49b3190fda664448b2f8db1362a904746
Sunil-731820 <sunilkumargupta1170@gmail.com>
2024-12-04 20:21:58 +0530

    Added The code for the API GateWay method

--- EurekaServerUsingAPIGateWay/EurekaServerUsingAPIGateWay/src/main/java/com/java/spr/EurekaServerUsingApiGateWayApplication.java
+++ EurekaServerUsingAPIGateWay/EurekaServerUsingAPIGateWay/src/main/java/com/java/spr/EurekaServerUsingApiGateWayApplication.java
@@ -2,8 +2,10 @@ package com.java.spr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerUsingApiGateWayApplication {
 
 	public static void main(String[] args) {
