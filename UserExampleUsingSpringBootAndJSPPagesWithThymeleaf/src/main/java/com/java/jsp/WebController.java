a164c34e0226553f238fc7c750480cb5eb4b8bae
Sunil-731820 <sunilkumargupta1170@gmail.com>
2024-12-09 14:45:54 +0530

    Code Added for JSP pages With Spring Boot With Example

--- UserExampleUsingSpringBootAndJSPPagesWithThymeleaf/src/main/java/com/java/jsp/WebController.java
+++ UserExampleUsingSpringBootAndJSPPagesWithThymeleaf/src/main/java/com/java/jsp/WebController.java
@@ -1,12 +1,24 @@
 package com.java.jsp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
 
 @Controller
 public class WebController {
+	
+	@Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/create-user")
     public String showCreateUserPage() {
@@ -25,5 +37,16 @@ public class WebController {
         // Example: restTemplate.postForObject(apiUrl, newUser, User.class);
 
         return "redirect:/user-list"; // Redirect to the user list page after saving
+    }
+    
+    @GetMapping("/user-list")
+    public String getUserList(Model model) {
+        // Fetch the list of users via the REST API
+        ResponseEntity<List<User>> response = restTemplate.exchange(
+                "http://localhost:8080/api/users/all", HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<User>>() {});
+        
+        model.addAttribute("users", response.getBody());
+        return "user-list";
     }
 }
